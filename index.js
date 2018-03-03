a74be4121caece27edbf118c93ac74f882664a2d
Jirat Onaree <jirat.onaree@gmail.com>
2018-03-03 17:19:16 +0700

    After android -> making it work on iOS too

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+
+// Component in case of stupid dump shit thing happen
+// import React from 'react';
+// import { TouchableOpacity, Text } from 'react-native';
+
+// const App = () => <TouchableOpacity><Text>lolsaldkfjsdlkfjasdlkfj</Text></TouchableOpacity>;
 
 AppRegistry.registerComponent('GifeApp', () => App);
