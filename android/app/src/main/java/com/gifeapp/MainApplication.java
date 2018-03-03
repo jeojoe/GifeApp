1b92cb6822affe54c9701b4148cf9a73e238873f
Jirat Onaree <jirat.onaree@gmail.com>
2018-03-03 15:37:40 +0700

    Install fbsdk (still broken) bc this android:TextAppearance.Material.Widget.Button.Borderless.Colored

--- android/app/src/main/java/com/gifeapp/MainApplication.java
+++ android/app/src/main/java/com/gifeapp/MainApplication.java
@@ -2,7 +2,12 @@ package com.gifeapp;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
@@ -15,6 +20,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -25,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new LinearGradientPackage(),
             new LinearGradientPackage()
       );
