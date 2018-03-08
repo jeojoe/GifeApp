22d4e46fdc49e232571e5c5038391d84ef31aebc
Jirat Onaree <jirat.onaree@gmail.com>
2018-03-08 22:49:37 +0700

    Create baseStyle and style const

---
+++ src/_constants/stylesConst.js
@@ -0,0 +1,1 @@
+export const horizontalPad = 20;
---
+++ src/_utils/baseStyles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import { sConst } from '../_constants';
+import Colors from './colors';
+
+export default StyleSheet.create({
+  wrapper: {
+    paddingTop: 30,
+    paddingHorizontal: sConst.horizontalPad,
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  horizontalPad: {
+    paddingHorizontal: sConst.horizontalPad,
+  },
+  header: {
+    fontWeight: 'bold',
+    paddingHorizontal: 32,
+    fontSize: 22,
+    marginBottom: 15,
+  },
+  headerFront: {
+    color: Colors.textGrey,
+    marginRight: 100,
+  },
+});
