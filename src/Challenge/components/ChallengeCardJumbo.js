edb0922a3f13e0cb9f729a48c7c8977a4607f213
Jirat Onaree <jirat.onaree@gmail.com>
2018-03-09 00:25:23 +0700

    lol Add title and gradient bg

--- src/Challenge/components/ChallengeCardJumbo.js
+++ src/Challenge/components/ChallengeCardJumbo.js
@@ -1,9 +1,11 @@
 // @flow
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import TimeLeftLabel from './TimeLeftLabel';
 import s, { cardHeight, cardWidth } from './ChallengeCardJumbo.style';
+import { Colors } from '../../_utils';
 
 type Props = {
   title: string,
@@ -16,14 +18,37 @@ const ChallengeCardJumbo = (props: Props) => (
     style={s.imageWrapper}
     source={{ uri: props.picture }}
   >
+    {/* Time */}
     <View style={s.timeLeft}>
       <TimeLeftLabel
         endDate={props.endDate}
       />
     </View>
-    <Text style={s.title}>
-      {props.title}
-    </Text>
+
+    {/* Header */}
+    <View style={s.flexSpace}>
+      <LinearGradient
+        colors={['transparent', '#000']}
+        start={{ x: 0.5, y: 0 }} end={{ x: 0.5, y: 1 }}
+        style={s.headerWrapper}
+      >
+        {/* Title */}
+        <Text style={s.title} numberOfLines={3}>
+          {props.title}
+        </Text>
+        {/* Location + Star */}
+
+      </LinearGradient>
+    </View>
+
+    {/* Reward */}
+    <LinearGradient
+      colors={[Colors.rewardBgLeft, Colors.rewardBgRight]}
+      start={{ x: 0, y: 1 }} end={{ x: 1, y: 0 }}
+      style={s.rewardWrapper}
+    >
+      <Text style={s.rewardText} numberOfLines={1}>ที่พักในโรงแรม HQO 1 คืน ฟรี!</Text>
+    </LinearGradient>
   </ImageBackground>
 );
 
--- src/Challenge/components/ChallengeCardJumbo.style.js
+++ src/Challenge/components/ChallengeCardJumbo.style.js
@@ -5,6 +5,8 @@ const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window'
 export const cardHeight = viewportWidth * 0.865;
 export const cardWidth = cardHeight;
 
+const innerHorizontalPad = '7%';
+
 export default StyleSheet.create({
   imageWrapper: {
     height: cardHeight,
@@ -12,12 +14,35 @@ export default StyleSheet.create({
     borderRadius: 10,
     overflow: 'hidden',
   },
-  title: {
-    backgroundColor: 'transparent',
-  },
   timeLeft: {
     position: 'absolute',
     right: 15,
     top: 12,
+  },
+  flexSpace: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  // Header
+  headerWrapper: {
+    paddingBottom: 10,
+    paddingTop: 72,
+    paddingHorizontal: innerHorizontalPad,
+  },
+  title: {
+    backgroundColor: 'transparent',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  rewardWrapper: {
+    height: 47,
+    justifyContent: 'center',
+    paddingHorizontal: innerHorizontalPad,
+  },
+  rewardText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
