d0630ed5b246c45b830973c236fd956020b8c1a7
Jirat Onaree <jirat.onaree@gmail.com>
2018-03-08 22:50:32 +0700

    Register

--- src/_constants/index.js
+++ src/_constants/index.js
@@ -1,9 +1,11 @@
 import * as actionTypes from './actionTypes';
 import * as alertMessages from './alertMessages';
 import Config from './tempConfig';
+import * as sConst from './stylesConst';
 
 export {
   actionTypes,
   alertMessages,
   Config,
+  sConst,
 };
--- src/_utils/index.js
+++ src/_utils/index.js
@@ -1,9 +1,11 @@
 import * as API from './apiCaller';
 import Colors from './colors';
 import * as GlobalActions from './globalActions';
+import sBase from './baseStyles';
 
 export {
   API,
   Colors,
   GlobalActions,
+  sBase,
 };
