f62871befc9f7f3e1923d2a51dc47f6043ab5eea
Jirat Onaree <jirat.onaree@gmail.com>
2018-03-10 22:17:06 +0700

    Fix critical bug with vector icon #626!! :fire:

---
+++ rn-cli.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+const blacklist = require('metro/src/blacklist')
+module.exports = {
+  getBlacklistRE () {
+    return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/])
+  },
+}
